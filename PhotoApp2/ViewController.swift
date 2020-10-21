2ba62552bf52568c6b0b1d589addfaafa6f3c4ee
sonokayashiro <scsshyashiro@gmail.com>
2020-10-21 14:52:56 +0900

    Initial Commit

---
+++ PhotoApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp2
+//
+//  Created by 家城苑佳 on 2020/10/21.
+//  Copyright © 2020 sonoka.yashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
